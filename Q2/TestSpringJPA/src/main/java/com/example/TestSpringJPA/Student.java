93a2dee5780d3cedd0321af07018e3036321bd59
Rounak-01-Hritu <it23037@mbstu.ac.bd>
2026-02-16 16:13:06 +0600

    Add files via upload

---
+++ Q2/TestSpringJPA/src/main/java/com/example/TestSpringJPA/Student.java
@@ -0,0 +1,49 @@
+package com.example.TestSpringJPA;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // Primary Key (Auto)
+
+    private String studentName;
+    private String studentId;
+    private Double cgpa;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public Double getCgpa() {
+        return cgpa;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setCgpa(Double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
